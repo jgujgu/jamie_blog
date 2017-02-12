ae6de7fbaca56cfb0f466aa9770dd04c36cc1498
jgu <jgu@jeffreygu.com>
2017-02-12 11:13:26 -0700

    add basic files

---
+++ config/initializers/monologue.rb
@@ -0,0 +1,14 @@
+Monologue.config do |config|
+  config.site_name = "My blog"
+  config.site_subtitle = "my own place online"
+  config.site_url = "http://example.com"
+
+  config.meta_description = "This is my blog about..."
+  config.meta_keyword = "music, fun"
+
+  config.admin_force_ssl = false
+  config.posts_per_page = 10
+  config.preview_size = 1000
+
+  #config.disqus_shortname = "my_disqus_shortname"
+end
